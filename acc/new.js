704a949ab91639304b735f95695cf66d92eefba7
jiaxiangkai <jiaxk@zenmen.com>
2019-11-29 18:21:44 +0800

    add  double-pointer quick-sort

--- acc/new.js
+++ acc/new.js
@@ -805,3 +805,51 @@ console.log(
   optimizeQuickSort([2, 5, 3, 6, 88, 22, 11, 11, 11, 11, 30, 50, 44])
 );
 console.timeEnd("test1");
+
+/**
+ * 快速排序，双指针方法
+ */
+//元素交换
+function swap(A, i, j) {
+  const t = A[i];
+  A[i] = A[j];
+  A[j] = t;
+}
+/**
+ *
+ * @param {*} A  数组
+ * @param {*} p  起始下标
+ * @param {*} r  结束下标 + 1
+ */
+function divide(A, p, r) {
+  const x = A[r - 1];
+  let i = p - 1;
+
+  for (let j = p; j < r - 1; j++) {
+    if (A[j] <= x) {
+      i++;
+      swap(A, i, j);
+    }
+  }
+
+  swap(A, i + 1, r - 1);
+
+  return i + 1;
+}
+/**
+ *
+ * @param {*} A  数组
+ * @param {*} p  起始下标
+ * @param {*} r  结束下标 + 1
+ */
+function qsort(A, p = 0, r) {
+  r = r || A.length;
+
+  if (p < r - 1) {
+    const q = divide(A, p, r);
+    qsort(A, p, q);
+    qsort(A, q + 1, r);
+  }
+
+  return A;
+}
