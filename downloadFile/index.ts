de1496ed8bcc18eeb8c555b21388f609c9d1032d
贾祥凯 <xiangkai.jia@eoitek.com>
2020-05-05 21:24:04 +0800

    ✨feat: 流处理文件下载并解决接口异常场景

---
+++ downloadFile/index.ts
@@ -0,0 +1,70 @@
+import axios from 'axios';
+import { resolve } from 'dns';
+
+/**
+ * 
+ * @param params 
+ * api
+ */
+function download(params: any) {
+  return axios.post('url', params), {
+    responseType: 'blob'
+  }
+}
+
+/**
+ * 下载
+ */
+async function export() {
+  const formDate = new FormData();
+  formDate.append("xxx", "xxx");
+  const res = await download(formDate);
+  blobToText(res.data).then(result => {
+    console.error(result.message)
+  }).catch(e => {
+    downloadFile(res)
+  })
+}
+
+/**
+ * 
+ * @param res 
+ * 
+ */
+function downloadFile(res: any) {
+  const title = decodeURIComponent(res.headers['content-disposition'].split(';')[1].split('=')[1]);
+  const blob = new Blob([res.data]);
+  const downloadUrl = URL.createObjectURL(blob);
+
+  let a = document.createElement('a');
+  a.style.display = 'none';
+  a.href = downloadUrl;
+  a.setAttribute('download', title);
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
+
+/**
+ * 
+ * @param blob 
+ * blob 当接口报错时,会生成text文件，此时读取text文件拦截下载文件流,处理异常
+ */
+function blobToText(blob: Blob): Promise<File | Blob> {
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.readAsText(blob);
+    fileReader.onload = function() {
+      try { 
+        const result = JSON.parse(this.result);
+        if(result && result.code !== 200) {
+          resolve(result)
+        } else {
+          reject()
+        }
+      } catch(e) {
+        reject()
+      }
+    }
+  })
+}
